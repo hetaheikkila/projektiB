d6a7b94fc89ce73a9a8bfea692478b728320cc68
Heta <hhmj28@gmail.com>
2024-11-05 09:26:37 +0200

    second commit

---
+++ ruokasovelluss/src/main/java/s24/ruokasovelluss/RuokasovellussApplication.java
@@ -0,0 +1,13 @@
+package s24.ruokasovelluss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuokasovellussApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuokasovellussApplication.class, args);
+	}
+
+}
---
+++ ruokasovelluss/src/test/java/s24/ruokasovelluss/RuokasovellussApplicationTests.java
@@ -0,0 +1,13 @@
+package s24.ruokasovelluss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuokasovellussApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
